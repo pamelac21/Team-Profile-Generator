e9473c1432f8535ef1e1f31c3ff6718c2528eabe
PamelaCleveland <pamelac021@gmail.com>
2021-10-09 20:36:56 -0400

    lib skeleton

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer');
 const generateHTML = require("./dist/generateHTML");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
